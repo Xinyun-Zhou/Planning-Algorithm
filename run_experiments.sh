263b371be63d349fd4119f23b4e1ee580dcf57e1
Xinyun Zhou <Xinyun-Zhou@outlook.com>
2023-09-07 16:51:47 +1000

    upload files

---
+++ run_experiments.sh
@@ -0,0 +1,135 @@
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem01.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem02.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem03.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem04.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem05.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem06.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem07.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/miconic/domain.pddl benchmarks/miconic/problem08.pddl miconic_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem01.pddl depot_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem02.pddl depot_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem07.pddl depot_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/depot/domain.pddl benchmarks/depot/problem03.pddl depot_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem04.pddl blocks_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem05.pddl blocks_temp 1:30:1 -x serial -l both -p true
+
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x parallel -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x serial -l both -p false
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x parallel -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x serial -l fmutex -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x parallel -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x serial -l reachable -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x parallel -l both -p true
+timeout 100 python3 -u planner.py benchmarks/blocks/domain.pddl benchmarks/blocks/problem06.pddl blocks_temp 1:30:1 -x serial -l both -p true
+
